396c43ccbfc0e02a9d89795192bbf59a1591ce5c
Roland Walker <walker@pobox.com>
2012-12-05 14:57:09 -0500

    defgroup links

--- syntactic-sugar.el
+++ syntactic-sugar.el
@@ -129,7 +129,9 @@
 (defgroup syntactic-sugar nil
   "Effect-free forms for clarification"
   :version "0.9.2"
-  :link '(emacs-commentary-link "syntactic-sugar")
+  :link '(emacs-commentary-link :tag "Commentary" "syntactic-sugar")
+  :link '(url-link :tag "Github" "http://github.com/rolandwalker/syntactic-sugar")
+  :link '(url-link :tag "EmacsWiki" "http://emacswiki.org/emacs/SyntacticSugar")
   :prefix "syntactic-sugar-"
   :group 'extensions)
 
